6b3452b96daf7fa754914042f5f9ebd76a1aa3ea
0xAurelou <0xaurelou@gmail.com>
2025-05-18 18:26:39 +0700

    init: aave v3 pt roll over dex initialization

---
+++ src/dex/aave-v3-pt-roll-over/config.ts
@@ -0,0 +1,9 @@
+import { DexParams } from './types';
+import { DexConfigMap } from '../../types';
+import { Network, SwapSide } from '../../constants';
+
+export const AaveV3PtRollOverConfig: DexConfigMap<DexParams> = {
+  AaveV3PtRollOver: {
+    // TODO: complete me!
+  },
+};
