751d75abb88e195a4375fdf0247eac77ca6bb973
Danylo Kanievskyi <danylokaniev@gmail.com>
2025-06-17 14:16:55 +0300

    chore: removed redundant test cases

--- tests/price-route.test.ts
+++
@@ -1,9 +0,0 @@
-import priceRouteData from './price-route.json';
-import { testPriceRoute } from './utils-e2e';
-import { OptimalRate } from '@paraswap/core';
-
-describe('Price Route', function () {
-  it('should work', async () => {
-    await testPriceRoute(priceRouteData.data.priceRoute as OptimalRate);
-  });
-});
