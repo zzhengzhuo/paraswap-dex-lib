4fc202e9b042a5a69d0642512e81f1cb98f02c9e
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-06-21 00:50:07 +0300

    feat: add `Unichain` support for `UniswapV4`

--- src/dex/uniswap-v4/uniswap-v4-e2e.test.ts
+++ src/dex/uniswap-v4/uniswap-v4-e2e.test.ts
@@ -321,4 +321,59 @@ describe('UniswapV4 E2E', () => {
       );
     });
   });
+
+  describe('Unichain', () => {
+    const network = Network.UNICHAIN;
+
+    describe('ETH -> USDC', () => {
+      const tokenASymbol: string = 'ETH';
+      const tokenBSymbol: string = 'USDC';
+
+      const tokenAAmount: string = '10000000000000000';
+      const tokenBAmount: string = '100000000';
+
+      testForNetwork(
+        network,
+        dexKey,
+        tokenASymbol,
+        tokenBSymbol,
+        tokenAAmount,
+        tokenBAmount,
+      );
+    });
+
+    describe('WETH -> USDC', () => {
+      const tokenASymbol: string = 'WETH';
+      const tokenBSymbol: string = 'USDC';
+
+      const tokenAAmount: string = '10000000000000000';
+      const tokenBAmount: string = '10000000';
+
+      testForNetwork(
+        network,
+        dexKey,
+        tokenASymbol,
+        tokenBSymbol,
+        tokenAAmount,
+        tokenBAmount,
+      );
+    });
+
+    describe('USDC -> DAI', () => {
+      const tokenASymbol: string = 'USDC';
+      const tokenBSymbol: string = 'DAI';
+
+      const tokenAAmount: string = '10000000';
+      const tokenBAmount: string = '1000000000000000000';
+
+      testForNetwork(
+        network,
+        dexKey,
+        tokenASymbol,
+        tokenBSymbol,
+        tokenAAmount,
+        tokenBAmount,
+      );
+    });
+  });
 });
